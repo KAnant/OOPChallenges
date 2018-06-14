400b27d3d327ef9c56a61d9a5ecbe0f361e6abc1
Anant <you@example.com>
2018-06-14 11:48:57 +0545

    Second Commit

---
+++ FibonacciSeq/Lis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lis
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "5", "2", "7", "4", "3", "8"};
+            List<int> res = new List<int>();
+            int arrLen = arr.Length;
+            int i = 0;
+            for (i = 0; i < arrLen; i++)
+            {
+                if (i < arrLen)
+                {
+                    if (res.Count != 0)
+                    {
+                        if (Convert.ToInt32(arr[i]) > res[res.Count - 1])
+                        {
+                            res.Add(Convert.ToInt32(arr[i]));
+                        }
+                    }
+                    else
+                    {
+                        res.Add(Convert.ToInt32(arr[i]));
+                    }
+                }
+            }
+
+            int len = res.Count;
+            Console.Write(len);
+            Console.ReadKey();
+        }
+    }
+}
