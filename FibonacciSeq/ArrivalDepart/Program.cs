60e9631a6424d2be24e7381ccc6c103175bd4c62
Anant <you@example.com>
2018-06-14 09:22:04 +0545

    Initial Commit

---
+++ FibonacciSeq/ArrivalDepart/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrivalDepart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter h1");
+            int arrivalHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter m1");
+            int arrivalMin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter h2");
+            int departureHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter m2");
+            int departureMin = Convert.ToInt32(Console.ReadLine());
+            TimeSpan t1 = new TimeSpan(arrivalHour,arrivalMin,0);
+            TimeSpan t2 = new TimeSpan(departureHour,departureMin,0);
+            TimeSpan totaltime = t1+t2;
+            Console.WriteLine(totaltime);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ FibonacciSeq/FibonacciSeq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSeq
+{
+    public class Fibo
+    {
+
+        static void Main(string[] args)
+            {
+                int n1 = 0;
+                int n2 = 1;
+                int r  ;
+                List<int> fibonoList = new List<int>();
+                for (int i = 0; i < 5; i++)
+                 {
+                     fibonoList.Add(n2);
+                     Console.WriteLine(n2);
+                     r = n2;
+                     n2 = n1 + n2;
+                     n1 = r;
+                     
+                }
+                Console.WriteLine("Enter a number");
+                int number = int.Parse(Console.ReadLine());
+                if (fibonoList.Contains(number))
+                {
+                    Console.WriteLine("IsFibo");
+                }
+                else
+                {
+                    Console.WriteLine("IsNotFibo");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
